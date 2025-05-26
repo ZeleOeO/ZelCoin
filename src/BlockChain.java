c665ac28ef120243e52593aac3e4d1baf7269885
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-26 10:26:12 +0100

    style: reformat code

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -5,12 +5,14 @@ public class BlockChain {
     private static List<Block> blocks = new ArrayList<Block>(
             new ArrayList<>(List.of(new Block("0", "Genesis")))
     );
-    private BlockChain() {}
+
+    private BlockChain() {
+    }
 
     public static void addBlock(String data) {
         String previousHash = blocks.getLast().getHash();
-       Block block = new Block(previousHash, data);
-       blocks.add(block);
+        Block block = new Block(previousHash, data);
+        blocks.add(block);
     }
 
     public static List<Block> getBlocks() {
