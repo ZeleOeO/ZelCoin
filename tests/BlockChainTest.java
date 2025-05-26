09b6d6efe05dc856dd93b6aa698410858aaba12d
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-26 10:23:56 +0100

    test: add test for block and blockchain class

---
+++ tests/BlockChainTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BlockChainTest {
+
+    @Test
+    public void testGenesisBlock() {
+        List<Block> blocks = BlockChain.getBlocks();
+        assertEquals(1, blocks.size(), "Blockchain should always start with Genesis block");
+        assertEquals("Genesis", blocks.getFirst().getData(), "First block should have data 'Genesis'");
+    }
+
+    @Test
+    public void testAddBlock() {
+        int initialSize = BlockChain.getBlocks().size();
+        BlockChain.addBlock("Block 1 Data");
+        BlockChain.addBlock("Block 2 Data");
+
+        List<Block> blocks = BlockChain.getBlocks();
+
+        assertEquals(initialSize + 2, blocks.size(), "Blockchain size should increase after adding blocks");
+
+        Block lastBlock = blocks.getLast();
+        Block secondLastBlock = blocks.get(blocks.size() - 2);
+
+        assertEquals("Block 2 Data", lastBlock.getData());
+        assertEquals(secondLastBlock.getHash(), lastBlock.getPrevHash(), "Previous hash should match the hash of the previous block");
+    }
+}
---
+++ tests/BlockTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+    Block block;
+
+    @Test
+    void createBlock() {
+        block = new Block("0", "Hello");
+        System.out.println(block.getHash());
+        assertEquals("0", block.getPrevHash());
+        assertEquals("Hello", block.getData());
+    }
+}
