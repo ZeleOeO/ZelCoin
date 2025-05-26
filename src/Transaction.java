3a301e1f41f18152d928df3eee4c938aeddec7b5
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-26 15:31:07 +0100

    feat: toString method override for transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,16 @@
 import java.security.PublicKey;
+import java.util.Base64;
 
 public record Transaction(Double amount, PublicKey sender, PublicKey receiver) {
+    @Override
+    public String toString() {
+        String senderKey = Base64.getEncoder().encodeToString(sender.getEncoded());
+        String receiverKey = Base64.getEncoder().encodeToString(receiver.getEncoded());
+
+        return "Transaction{" +
+               "\n amount=" + amount +
+               ",\n sender=" + senderKey +
+               ",\n receiver=" + receiverKey +
+               "\n }";
+    }
 }
